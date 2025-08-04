133663df9af2ae9ccabb9f3b868d17c2c6d6d81d
THARUN VARDHINENI <tharunvardhineni@gmail.com>
2025-08-04 21:53:02 +0000

    push

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import DashboardLayout from "../Layouts/DashboardLayout";
 import Projects from "../pages/Projects";
 import Skills from "../pages/Skills";
 import Home from "../pages/Home";
-import ReconDetails from '../pages/Recon_details';
+import ReconDetails from "../pages/Recon_details";
 import ADMDetails from "../pages/ADM_details";
-import Fito_details from "../pages/Fito_details";
-import Vetro_details from "../pages/Vetro_details";
-import Insurance_portal from "../pages/Insurance_portal";
+import FitoDetails from "../pages/Fito_details";
+import VetroDetails from "../pages/Vetro_details";
+import InsurancePortal from "../pages/Insurance_portal";
 
 export default function AppRouter() {
   return (
-    <Router basename="/"> 
+    <Router>
       <Routes>
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route index element={<Navigate to="/dashboard/home" replace />} />
+        <Route path="/" element={<DashboardLayout />}>
+          <Route index element={<Navigate to="home" replace />} />
           <Route path="home" element={<Home />} />
           <Route path="projects" element={<Projects />} />
           <Route path="skills" element={<Skills />} />
           <Route path="smart-recon" element={<ReconDetails />} />
           <Route path="atm-monitoring" element={<ADMDetails />} />
-          <Route path="fito" element={<Fito_details />} />
-          <Route path="vetro" element={<Vetro_details />} />
-          <Route path="insurance-portal" element={<Insurance_portal />} />
+          <Route path="fito" element={<FitoDetails />} />
+          <Route path="vetro" element={<VetroDetails />} />
+          <Route path="insurance-portal" element={<InsurancePortal />} />
         </Route>
 
-        <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+        <Route path="*" element={<Navigate to="home" replace />} />
       </Routes>
     </Router>
   );
