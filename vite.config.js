ac5e891057d5fb0e108663ecae19e7ece3b065f5
THARUN VARDHINENI <tharunvardhineni@gmail.com>
2025-08-04 20:01:05 +0000

    modified package.json and vite config pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',   // IMPORTANT: relative base for custom domain root
   plugins: [react()],
-  base: '/',  // Set the base path here for GitHub Pages
 });
