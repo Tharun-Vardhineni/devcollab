86ab55a854f1c1b636439b9e8e7f6ac611966f52
THARUN VARDHINENI <tharunvardhineni@gmail.com>
2025-08-04 20:44:02 +0000

    modified routes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 import { FaExternalLinkAlt } from "react-icons/fa";
 const projects = [
   {
@@ -112,13 +113,13 @@ export default function Projects() {
             <div className="absolute inset-x-0 bottom-0 h-0 group-hover:h-1/2 transition-all duration-500 ease-in-out bg-gray-800/40 backdrop-blur-md z-20 flex items-end pointer-events-none group-hover:pointer-events-auto">
               <div className="w-full flex justify-center mb-6 transform translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500">
                 {/* For View Details button */}
-<a
+<Link
   href={caseStudy}  
   className="px-5 py-2 text-sm rounded-xl text-white font-medium border border-green-400 hover:bg-green-500 hover:text-white bg-white/10 backdrop-blur-xl"
 >
   
   View Details
-</a>
+</Link>
 
               </div>
             </div>
